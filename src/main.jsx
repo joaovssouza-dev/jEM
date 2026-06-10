de0f7a602cf60af96a18c4fee3d8ec5c84737425
João Vitor S. Souza <jv.santiago.souza@gmail.com>
2026-06-10 02:12:11 -0300

    Fix: import App

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import './index.css';
 
-root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
